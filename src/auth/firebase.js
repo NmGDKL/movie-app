d8b163cdbca89f17f421bebc6e96cb32478a0a04
Naim <naimgedikli01@gmail.com>
2022-04-14 15:06:25 +0300

    first commit.

---
+++ src/auth/firebase.js
@@ -0,0 +1,102 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBIpFvOG-FAV7SwOEEEfWCbB1CLUsjavlY",
+//   authDomain: "movie-app-ff33a.firebaseapp.com",
+//   projectId: "movie-app-ff33a",
+//   storageBucket: "movie-app-ff33a.appspot.com",
+//   messagingSenderId: "861745453702",
+//   appId: "1:861745453702:web:88e3ae7080b39c8cf38f07",
+// };
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export const createUser = async (email, password, displayName, navigate) => {
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
+export const signIn = async (email, password, navigate) => {
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export const logOut = () => {
+  signOut(auth);
+  alert("logged out successfully");
+};
+
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+      // ...
+    } else {
+      // User is signed out
+      setCurrentUser(false);
+    }
+  });
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Google
+export const signUpProvider = (navigate) => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+      // ...
+    });
+};
